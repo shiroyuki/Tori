f2f5ca850a2dcc9802957b26c32f8a4f0b9a7971
Juti Noppornpitak <jnopporn@shiroyuki.com>
2012-10-11 00:07:21 -0400

    Added the changeset export for the mapper.

--- test/testcase/test_db_odm.py
+++ test/testcase/test_db_odm.py
@@ -6,6 +6,11 @@ from tori.db.odm.document  import Document
 class Person(Document): pass
 
 class TestDocument(unittest.TestCase):
+    test_attributes = {
+        'name': 'Juti',
+        'occupation': 'Developer'
+    }
+
     def setUp(self):
         pass
 
@@ -13,43 +18,39 @@ class TestDocument(unittest.TestCase):
         pass
 
     def test_new_document(self):
-        person = Person(name='Juti', occupation='Software Developer')
+        person = Person(**self.test_attributes)
 
         self.assertEquals('person', person.collection_name)
 
         self.assertEquals(None, person.id)
         self.assertEquals('Juti', person.name)
         self.assertFalse(person.is_dirty('name'))
+        self.assertEquals('test_db_odm.Person', person.get_class_name())
+
+    def test_new_document_changeset(self):
+        person = Person(**self.test_attributes)
+
+        changeset = person.get_changeset()
+
+        for name in self.test_attributes:
+            self.assertTrue(name in changeset)
+            self.assertEquals(self.test_attributes[name], changeset[name])
+
+        self.assertTrue(changeset)
 
     def test_old_document(self):
-        person = Person(_id=1, name='Juti', occupation='Software Developer')
+        person = Person(_id=1, **self.test_attributes)
 
         self.assertEquals(1, person.id)
+        self.assertFalse(person.get_changeset())
 
-        with self.assertRaises(LockedIdException):
-            person._id = 2
-
-    def test_old_document_on_updating_existing_attribute(self):
-        person = Person(_id=1, name='Juti', occupation='Software Developer')
+    def test_old_document_on_adding_new_attribute(self):
+        person = Person(_id=1, **self.test_attributes)
 
         self.assertEquals(1, person.id)
-        self.assertFalse(person.is_dirty('name'))
-
-        person.name = 'Obiwan'
-
-        self.assertEquals('Obiwan', person.name)
-        self.assertTrue(person.is_dirty('name'))
-
-        with self.assertRaises(ReservedAttributeException):
-            person.is_dirty = 2
 
         with self.assertRaises(AttributeError):
-            self.assertFalse(person.is_dirty('is_dirty'))
-
-    def test_old_document(self):
-        person = Person(_id=1, name='Juti', occupation='Software Developer')
-
-        self.assertEquals(1, person.id)
+            self.assertFalse(person.is_dirty('is_awesome'))
 
         with self.assertRaises(AttributeError):
             person.is_dirty('is_awesome')
@@ -58,3 +59,48 @@ class TestDocument(unittest.TestCase):
 
         self.assertTrue(person.is_awesome)
         self.assertTrue(person.is_dirty('is_awesome'))
+
+    def test_old_document_on_updating_existing_attribute(self):
+        person = Person(_id=1, **self.test_attributes)
+
+        self.assertFalse(person.is_dirty('name'))
+
+        person.name = 'Obiwan'
+
+        self.assertEquals('Obiwan', person.name)
+        self.assertTrue(person.is_dirty('name'))
+
+    def test_old_document_on_updating_existing_attribute_with_correct_changeset(self):
+        person = Person(_id=1, **self.test_attributes)
+
+        person.name = 'Obiwan'
+
+        changeset = person.get_changeset()
+
+        self.assertTrue('_id' in changeset)
+        self.assertTrue('name' in changeset)
+        self.assertFalse('occupation' in changeset)
+
+    def test_old_document_failed_on_attempt_to_change_id(self):
+        person = Person(_id=1, **self.test_attributes)
+
+        with self.assertRaises(LockedIdException):
+            person.id = 2
+
+        with self.assertRaises(ReservedAttributeException):
+            person._id = 2
+
+    def test_document_failed_to_update_reserved_attributes(self):
+        person = Person(_id=1, **self.test_attributes)
+
+        with self.assertRaises(ReservedAttributeException):
+            person.is_dirty = 2
+
+        with self.assertRaises(ReservedAttributeException):
+            person.collection_name = 'asdf'
+
+    def test_document_failed_to_check_unknown_attribute(self):
+        person = Person(_id=1, **self.test_attributes)
+
+        with self.assertRaises(AttributeError):
+            self.assertFalse(person.is_dirty('is_dirty'))
--- tori/db/odm/document.py
+++ tori/db/odm/document.py
@@ -9,8 +9,8 @@ class Document(object):
         for name in attributes:
             self.__dict__[name] = attributes[name]
 
-        self.__dict__['_dirty_attributes'] = []
-        self.__dict__['_collection_name']  = self.__class__.__name__.lower()
+        self.__dict__['__dirty_attributes__'] = []
+        self.__dict__['__collection_name__']  = self.__class__.__name__.lower()
 
     @property
     def id(self):
@@ -24,29 +24,61 @@ class Document(object):
         :param id: the ID of the document.
         '''
         if '_id' in self.__dict__ and self.__dict__['_id']:
-            raise LockedIdException('The ID is locked and cannot be changed.')
+            raise LockedIdException('The ID is already assigned and cannot be changed.')
 
         self.__dict__['_id'] = id
 
     @property
     def collection_name(self):
-        return self.__dict__['_collection_name']
+        return self.__dict__['__collection_name__']
+
+    def get_class_name(self):
+        '''
+        Retrieve the full type name, include
+        '''
+        return '{}.{}'.format(self.__module__, self.__class__.__name__)
 
     def is_dirty(self, name):
         if name not in self.__dict__:
             raise AttributeError('The attribute "%s" is not found.' % name)
 
-        return name in self._dirty_attributes
+        return name in self.__dirty_attributes__
 
-    def __setattr__(self, name, value):
-        if name[0] == '_'\
+    def get_changeset(self):
+        changeset = {}
+
+        attribute_names = self.__dict__['__dirty_attributes__'] if self.id else self.__dict__.keys()
+
+        for name in attribute_names:
+            if self.__is_reserved_attribute_name(name):
+                continue
+
+            changeset[name] = self.__dict__[name]
+
+        if changeset:
+            changeset.update({
+                '_id':   self.id,
+                '_type': self.get_class_name()
+            })
+
+        return changeset
+
+    def revert_changes(self):
+        self.__dict__['__dirty_attributes__'] = []
+
+    def __is_reserved_attribute_name(self, name):
+        return name[0:2] == '__'\
+            or name in ['_id', 'collection_name']\
             or (
                 name in dir(self)\
                 and callable(self.__getattribute__(name))
-            ):
+            )
+
+    def __setattr__(self, name, value):
+        if self.__is_reserved_attribute_name(name):
             raise ReservedAttributeException('"%s" is a reserved attribute.' % name)
 
-        if name not in self.__dict__['_dirty_attributes']:
-            self.__dict__['_dirty_attributes'].append(name)
-
         object.__setattr__(self, name, value)
+
+        if name not in self.__dict__['__dirty_attributes__']:
+            self.__dict__['__dirty_attributes__'].append(name)
