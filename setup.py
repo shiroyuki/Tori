093aab72911ca7c70bdf4f48d98aefb29b6f4ba8
Juti Noppornpitak <jutin@nationalfibre.net>
2013-01-11 11:15:45 -0500

    Prepare the setup script for Python 2.6+

--- setup.py
+++ setup.py
@@ -1,35 +1,12 @@
 from distutils.core import setup
-from glob           import glob
-from os.path        import abspath, dirname, join
-from re             import sub
-from sys            import exit
-
-module_name = 'tori'
-found_paths = glob(join(module_name, '*'))
-
-found_paths.append(module_name)
-
-packages = []
-
-for found_path in found_paths:
-    if '__'   in found_path: continue
-    if '.pyc' in found_path: continue
-    if '.py' in found_path: continue
-    
-    package = found_path
-    package = sub('\.py', '', package)
-    package = sub('/', '.', package)
-    
-    packages.append(package)
-
-packages.sort()
 
 setup(
     name         = 'Tori',
     version      = '2.0',
-    description  = 'Lightweight Web Framework',
+    description  = 'Micro Web Framework',
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
     url          = 'http://shiroyuki.com/work/projects-tori',
-    packages     = packages
+    packages     = ['tori', 'tori.data', 'tori.db', 'tori.decorator', 'tori.session', 'tori.socket', 'tori.template'],
+    scripts      = ['bin/nest']
 )
