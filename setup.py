6a573f0af7462b4d4da37f46c5c50a15b322aab4
Juti Noppornpitak <jnopporn@shiroyuki.com>
2013-03-06 11:18:06 -0500

    Fixed the setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,17 @@ setup(
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
     url          = 'http://shiroyuki.com/work/projects-tori',
-    packages     = ['tori', 'tori.data', 'tori.db', 'tori.decorator', 'tori.session', 'tori.socket', 'tori.template'],
+    packages     = [
+        'tori',
+        'tori.data',
+        'tori.db',
+        'tori.decorator',
+        'tori.session',
+        'tori.session.entity',
+        'tori.session.repository',
+        'tori.socket',
+        'tori.template'
+    ],
     scripts      = ['bin/nest'],
     install_requires = ['imagination', 'kotoba', 'tornado', 'jinja2', 'pymongo']
 )
