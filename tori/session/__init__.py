0949c4be674580f82f25f7b7026953dd0dedbd4c
Juti Noppornpitak <jnopporn@shiroyuki.com>
2014-11-21 22:14:31 -0500

    Added a shortcut

---
+++ tori/session/__init__.py
@@ -0,0 +1,4 @@
+from tori.session.repository.base   import BaseSessionAdapter
+from tori.session.repository.file   import FileSessionAdapter
+from tori.session.repository.memory import InMemorySessionAdapter
+from tori.session.repository.xredis import RedisSessionAdapter
